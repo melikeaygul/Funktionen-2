edecf4602b93e0dbf0d517af09bd8ba5b813c9a5
melikeaygul <melikeaygul0810@gmail.com>
2025-03-17 15:16:53 +0100

    Bonus aufgaben

--- funktionen.dart
+++ funktionen.dart
@@ -1,14 +1,13 @@
 //Aufgabe1
 int sum(int a, int b, int c) {
-  int result = a + b + c;
-  return result;
+  return a + b + c;
 }
 
 void main() {
   int result = sum(3, 6, 8);
   int result2 = sum(5, 9, 10);
   int result3 = sum(8, 9, 12);
-
+  //print(sum(3, 6, 8)); zsm gefasst möglich
   print(result);
   print(result2);
   print(result3);
